6b3252d8a3ac1e4e468a152095e0567218196fd5
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-09-23 13:39:20 +0200

    debug: log classnames for active state in navigation links

--- src/components/layout/mobile-nav.tsx
+++ src/components/layout/mobile-nav.tsx
@@ -29,6 +29,16 @@ function NavLink({
     const isActive = pathname === href
     console.log('Is Active:', isActive)
 
+    console.log(
+        'Classnames:',
+        cn(
+            isActive
+                ? 'bg-foreground/10'
+                : 'bg-foreground/0 text-muted-foreground',
+            'hover:text-foreground rounded-full px-4 py-2 font-medium transition-colors'
+        )
+    )
+
     return (
         <Link
             href={{ pathname: href }}
